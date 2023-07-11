b0a82155691cafeeef1a5d4b28500683964973ce
anns318 <an310898@gmai.com>
2023-07-11 16:17:28 +0700

    use custom hook for post method

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,46 +1,39 @@
-import { useState } from "react";
-
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
+import { useRef } from "react";
 
 const NewTask = props => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest } = useHttp();
+
+  const inputRef = useRef();
+
+  const applyData = data => {
+    const generatedId = data.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: inputRef.current.value };
+    console.log(createdTask);
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async taskText => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-http-1d49d-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
-        {
-          method: "POST",
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
+    const requestConfig = {
+      url: "https://react-http-1d49d-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+      method: "POST",
+      body: { text: taskText },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    sendRequest(requestConfig, applyData);
   };
 
   return (
     <Section>
-      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      <TaskForm
+        ref={inputRef}
+        onEnterTask={enterTaskHandler}
+        loading={isLoading}
+      />
       {error && <p>{error}</p>}
     </Section>
   );
--- src/components/NewTask/TaskForm.js
+++ src/components/NewTask/TaskForm.js
@@ -1,11 +1,11 @@
-import { useRef } from 'react';
+import React, { useRef } from "react";
 
-import classes from './TaskForm.module.css';
+import classes from "./TaskForm.module.css";
 
-const TaskForm = (props) => {
-  const taskInputRef = useRef();
+const TaskForm = React.forwardRef((props, ref) => {
+  const taskInputRef = ref;
 
-  const submitHandler = (event) => {
+  const submitHandler = event => {
     event.preventDefault();
 
     const enteredValue = taskInputRef.current.value;
@@ -17,10 +17,10 @@ const TaskForm = (props) => {
 
   return (
     <form className={classes.form} onSubmit={submitHandler}>
-      <input type='text' ref={taskInputRef} />
-      <button>{props.loading ? 'Sending...' : 'Add Task'}</button>
+      <input type="text" ref={ref} />
+      <button>{props.loading ? "Sending..." : "Add Task"}</button>
     </form>
   );
-};
+});
 
 export default TaskForm;
